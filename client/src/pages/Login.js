747eec4f71526838e0345a87e6881a7c61792693
Ansh Naikele <ansh.naikele2003@gmail.com>
2023-03-02 13:28:25 -0600

    add

---
+++ client/src/pages/Login.js
@@ -0,0 +1,60 @@
+import {useState} from 'react'
+import {useNavigate} from 'react-router-dom'
+
+function App() {
+    const navigate = useNavigate()
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    async function loginUser(event) {
+        event.preventDefault()
+
+        const response = await fetch('http://localhost:1337/api/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email,
+                password
+            }),
+        })
+
+        const data = await response.json();
+
+        if(data.user){
+            localStorage.setItem('token', data.user)
+            alert('Login successful')
+            navigate('/dashboard')
+        }
+        else{
+            alert('Please check your email and password')
+        }
+    }
+
+    return( 
+        <div>
+            <h1>Login</h1>
+            <form onSubmit = {loginUser}>
+                <input 
+                    value = {email}
+                    onChange = {(e) => setEmail(e.target.value)}
+                    type="text" 
+                    placeholder="Email" 
+                />
+                <br />
+                <input 
+                    value = {password}
+                    onChange = {(e) => setPassword(e.target.value)}
+                    type="text" 
+                    placeholder="Password" 
+                />
+                <br />
+                <input type="submit" value="Login"/>
+            </form>
+        </div>
+    )
+}
+
+export default App
